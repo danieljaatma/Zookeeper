c03f2bcda207b30fb0ea9dc77220add210785e5a
danieljaatma <daniel.jaatma@gmail.com>
2022-12-28 15:55:55 +0200

    Kolmas projekt.

---
+++ Topics/Primitive and reference types/Creating a primitive/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int yearOfRelease = 1996;
+        System.out.println(yearOfRelease);
+        // put your code here
+    }
+}
